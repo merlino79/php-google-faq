af67d895c634bbfe4b591923a42dc91a5c70ee31
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-07 16:05:36 +0200

    inizio

--- index.php
+++ index.php
@@ -4,10 +4,35 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
   <title>php-google-fack</title>
 </head>
 <body>
-prova
-  
+    <header>
+    <div class="header-top">
+       <div class="logo-img">
+
+          <img src="./assets/img/logo.jpg" alt="">
+      </div>   
+      <div class="logo-txt">
+        <h3>privacy e terminy</h3>
+      </div>
+
+
+       
+    </div>
+    <div class="header-bottom">
+    pem
+    </div>
+
+
+    </header>
+
+    <main>
+    </main>
+
+    <footer>
+    </footer>
+
 </body>
 </html>
