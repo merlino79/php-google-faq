62e283c02f62b6b5814ae22d57c4f16ee99d2cfb
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-07 17:51:02 +0200

    in lavorazione

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <title>php-google-fack</title>
 </head>
 <body>
@@ -16,6 +17,10 @@
       </div>   
       <div class="logo-txt">
         <h3>privacy e terminy</h3>
+      </div>
+      <div class="favicon">
+         <i class="fas fa-bars"></i>
+         <button class="btn">accedi</button>
       </div>
 
 
